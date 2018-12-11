73f2332ebff2756803aaeb6dfdc9827ae08fae16
Joseph.Barnes <joseph.barnes@inin.com>
2018-12-10 22:30:08 -0500

    meh

--- src/day7.ts
+++ src/day7.ts
@@ -3,7 +3,8 @@ import { chmod } from "fs";
 interface Cmd {
     name: string,
     before: string[],
-    after: string[]
+    after: string[],
+    done: number
 }
 
 class Day7 {
@@ -19,6 +20,7 @@ class Day7 {
 
     public static doPuzzles(input: string[]) {
         console.log(`runtime output - ${this.orderRuntime(input)}`);
+        console.log(`threaded output - ${this.multiThread(input)}`);
     }
 
     public static orderRuntime(input: string[]) {
@@ -41,7 +43,8 @@ class Day7 {
                 commands.push({
                     name: inst.first,
                     before: [],
-                    after: [inst.second]
+                    after: [inst.second],
+                    done: 0
                 });
             }
 
@@ -51,7 +54,8 @@ class Day7 {
                 commands.push({
                     name: inst.second,
                     before: [inst.first],
-                    after: []
+                    after: [],
+                    done: 0
                 });
             }
         }
@@ -66,12 +70,10 @@ class Day7 {
         // console.log(start);
         while(queue.length !== 0) {
             let current: Cmd = queue[0]
-            let index: number = 1;
             let noPredecessors: boolean = eval(current.before.map(c => {
                 return runtime.indexOf(c) >= 0;
             }).join(' && ')) || current.before.length === 0;
             // console.log(`${current.name} has no preds - ${noPredecessors}`);
-            let before: string[] = queue.map(c => c.name);
 
             if (noPredecessors) { // if current has no remaining befores
 
@@ -83,8 +85,7 @@ class Day7 {
                     for(let add of current.after) {
                         let next: Cmd = commands.find(cmd => cmd.name === add) as Cmd;
                         // console.warn(`${add} returns ${JSON.stringify(next)}`);
-
-                        if(queue.indexOf(next) === -1) queue.splice(index++,0,next);
+                        if(queue.indexOf(next) === -1) queue.push(next);
                     }
                 }
 
@@ -94,6 +95,11 @@ class Day7 {
 
                 //remove current from queue
                 queue.splice(0,1);
+
+                //alphabetize
+                queue.sort((a,b) => {
+                    return a.name < b.name ? -1 : 1;
+                });
             } else {
                 // console.log(`${current.name} waiting on ${}`)
                 queue.splice(0,1);
@@ -102,11 +108,100 @@ class Day7 {
                 // console.log(`moved first one spot - ${JSON.stringify(queue.map(c => c.name))}`);
             }
 
-            let after: string[] = queue.map(c => c.name)
-            // console.log(`${JSON.stringify(before)} -> ${JSON.stringify(after)} = ${runtime}`);
         }
 
         return runtime;
+    }
+
+    public static multiThread(input: string[]) {
+        let runtime: string = '';
+        let commands: Cmd[] = [];
+        let instReg: RegExp = /Step (\w) .+ step (\w) .+./;
+        let instructions: {first: string, second: string}[] = input.map(inst => {
+            let reg: RegExpExecArray = instReg.exec(inst) as RegExpExecArray,
+                [,first,second] = reg;
+            return {first, second};
+        });
+        
+        for(let inst of instructions) {
+            let fst: Cmd = commands.find(c => c.name === inst.first) as Cmd;
+            let snd: Cmd = commands.find(c => c.name === inst.second) as Cmd;
+
+            if(fst) {
+                commands[commands.indexOf(fst)].after.push(inst.second);
+            } else {
+                commands.push({
+                    name: inst.first,
+                    before: [],
+                    after: [inst.second],
+                    done: inst.first.charCodeAt(0) - 4
+                });
+            }
+
+            if(snd) {
+                commands[commands.indexOf(snd)].before.push(inst.first);
+            } else {
+                commands.push({
+                    name: inst.second,
+                    before: [inst.first],
+                    after: [],
+                    done: inst.second.charCodeAt(0) - 4
+                });
+            }
+        }
+
+        let starters: Cmd[] = commands.filter(cmd => cmd.before.length === 0);
+        let queue: Cmd[] = [];
+        queue = starters.sort((a,b) => {
+            return a.name < b.name ? -1 : 1;
+        });
+        // console.log(start);
+        let workers: Cmd[] = [];
+        let time: number = 0;
+
+
+        while(queue.length !== 0) {
+            let temp: Cmd[] = [];
+            for(let work of workers) { // pop out if timer is up
+                if(work.done >= time || work.name === 'fake') {
+                    runtime += work.name;
+                } else {
+                    temp.push(work);
+                }
+            }
+            workers = temp;
+
+            while(workers.length < 5) { // pull up to 5 commands off the queue
+                let current: Cmd = queue[0]
+                let noPredecessors: boolean = eval(current.before.map(c => {
+                    return runtime.indexOf(c) >= 0;
+                }).join(' && ')) || current.before.length === 0;
+
+                if (noPredecessors) { // if current has no remaining befores
+                    if (current.after.length !== 0) {
+                        current.after.sort((a,b) => {
+                            return a < b ? -1 : 1;
+                        });
+                        for(let add of current.after) {
+                            let next: Cmd = commands.find(cmd => cmd.name === add) as Cmd;
+                            if(queue.indexOf(next) === -1) queue.push(next);
+                        }
+                    }
+                    //remove current from queue, add to workers
+                    queue.splice(0,1);
+                    current.done = current.name.charCodeAt(0) - 4 + time;
+
+                    //alphabetize
+                    queue.sort((a,b) => {
+                        return a.name < b.name ? -1 : 1;
+                    });
+                } else {
+                    queue.splice(0,1);
+                    queue.push(current);
+                    if (queue[0] === queue[1]) break;
+                }
+            }
+        }
     }
 }
 
--- src/day8.ts
+++ src/day8.ts
@@ -5,25 +5,59 @@ interface Node {
 class Day8 {
     public static run() {
         const fs = require('fs');
-        fs.readFile('inputs/day8.txt', 'utf8', (err: Error, fileText: string) => {
+        fs.readFile('inputs/test8.txt', 'utf8', (err: Error, fileText: string) => {
             if (fileText) {
-                let instructions: string[] = fileText.split(' ');
+                let instructions: number[] = fileText.split(' ').map(a => parseInt(a));
                 this.doPuzzles(instructions);
             }
         });
     }
 
-    public static doPuzzles(input: string[]) {
+    public static doPuzzles(input: number[]) {
+        let dup: number[] = input.slice();
         console.log(`metadata output - ${this.metadata(input)}`);
+        console.log(`root val output - ${this.rootVal(dup)}`);
     }
 
-    public static metadata(input: string[]): number {
+    public static metadata(input: number[]): number {
         // call recursively, pass metadata sums back up the chain, return a number
         let sum: number = 0;
-
         
+        let numChildren: number = input.shift() as number;
+        let numMeta: number = input.shift() as number;
 
-        return sum
+        for (let n = 0; n < numChildren; n++) sum += this.metadata(input);
+
+        for (let n = 0; n < numMeta; n++) sum += input.shift() as number;
+
+        return sum;
+    }
+    
+    metaVals: number[] = []
+
+    public static rootVal(input: number[]): number {
+        let val: number = 0;
+        let state: number[] = input.slice();
+        let numChildren: number = input.shift() as number;
+        let numMeta: number = input.shift() as number;
+        let meta: number[] = input.slice(input.length - numMeta, numMeta); // can't do this!
+
+        if (numChildren === 0) {
+            val += meta.reduce((acc,num) => acc + num, 0);
+        } else {
+            meta.filter((n) => n <= numChildren);
+            for (let met of meta) {
+                for (let n = 0; n < numChildren; n++) {
+                    if(n === met) {
+                        let update: number = this.rootVal(input);
+                        val += update;
+                    }
+                }
+            }
+        }
+        console.log(`${state} - ${val}`);
+
+        return val;
     }
 }
 
